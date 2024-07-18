d5ee6dd74e30abc75b815d700ebca96d932d6f06
JawadAhbab <jawad.ahbab.turna@gmail.com>
2024-07-18 06:54:30 +0600

    🔥 Core

--- src/core/createMongoJson.js
+++ src/core/createMongoJson.js
@@ -5,10 +5,10 @@ export const createMongoJson = async () => {
   const tables = await tablenames()
   for (const table of tables) {
     if (configs.disableTables.includes(table)) {
-      console.log('CREATE'.padEnd(8), ':', table, '=>', 'DISABLED')
+      console.log('CREATE'.padEnd(8), ':', table, '=>', false)
       continue
     }
 
-    console.log(table)
+    console.log('CREATE'.padEnd(8), ':', table, '=>', true)
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import { createMongoJson } from './core/createMongoJson.js'
 
 createMongoJson()
 export const configs = {
-  disableTables: ['android_metadata'],
+  disableTables: [
+    'android_metadata',
+    't_favourite',
+    't_sponsored_brand',
+    'addvertisement',
+    'sqlite_sequence',
+    'district',
+    'thana',
+    'chamber_info',
+    'd_specialty',
+    'job',
+    'job_category_type',
+    'occupation',
+    't_addvertisement',
+  ],
 }
